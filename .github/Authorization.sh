b5f751f7b35c4e65a3db13169db04f9a79597ade
ejunds <102132258+ejunds@users.noreply.github.com>
2023-11-19 15:19:23 +0800

    Create Authorization.sh
    Signed-off-by: ejunds <102132258+ejunds@users.noreply.github.com>

---
+++ .github/Authorization.sh
@@ -0,0 +1,5 @@
+curl --request GET \
+--url "https://api.github.com/app" \
+--header "Accept: application/vnd.github+json" \
+--header "Authorization: Bearer YOUR_JWT" \
+--header "X-GitHub-Api-Version: 2022-11-28"
