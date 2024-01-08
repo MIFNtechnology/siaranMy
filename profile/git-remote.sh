5e2e7606d023e25151b235654f4b14b24abc1ba2
ejunds <102132258+ejunds@users.noreply.github.com>
2024-01-08 22:41:31 +0800

    Update git-remote.sh
    Signed-off-by: ejunds <102132258+ejunds@users.noreply.github.com>

--- profile/git-remote.sh
+++ profile/git-remote.sh
@@ -1,6 +1,5 @@
-git branch -m shell
+git branch -m main
 git fetch origin
 git branch -u main main
 git remote set-head origin -a
-git switch -c <siaraMy>
-git branch -m <main>
+git switch -c siaraMy
