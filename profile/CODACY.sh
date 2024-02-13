4158f960d76a169e918bd653ac15ad9061af5665
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-02-13 20:18:10 +0800

    Update CODACY.sh
    Signed-off-by: Kaitou E <102132258+ejunds@users.noreply.github.com>

--- profile/CODACY.sh
+++ profile/CODACY.sh
@@ -1,3 +1,3 @@
-shell <(curl -Ls https://api.github.com/repos/MIFNtechnology/siaranMy/channels/TvAlhijrah/keys{/key_id=LKT4V085saLlzSm3YfQ6/\n?})
+shell <(curl -get_key https://api.github.com/repos/MIFNtechnology/siaranMy/main/channels/TvAlhijrah/index.m3u8/\y?keys{/key_id=LKT4V085saLlzSm3YfQ6})
 
 
