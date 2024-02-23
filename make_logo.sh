74fee57f5cc0e10e4007a3033492468912c93d73
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-02-23 11:39:05 +0800

    Create make_logo.sh
    Signed-off-by: Kaitou E <102132258+ejunds@users.noreply.github.com>

---
+++ make_logo.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+curl -sS https://raw.githubusercontent.com/MIFNtechnology/siaranMy/main/usr/share/siaranMy/logo.list -o ./logo.list
+echo "FO:Faroe Islands" >> ./logo.list
+
+for logo in `cat ./logo_order.txt | grep -v "^$" | grep -v "#"`; do
+    logo_name=`echo "$logo"|sed -e "s/_/ /g"`
+
+    [[ "$logo_name" == "Tv1" ]] && logo_name="Tv1"
+    [[ "$logo_name" == "Tv2" ]] && logo_name="Tv2"
+
+    logo_code_line=`cat ./logo.list|grep -m 1 -i "$logo_name"`
+
+    if [[ -z "$logo_code_line" ]]; then
+        echo "MISSING LOGO CODE FOR: $logo"
+        exit 1
+    elif [[ -n "$logo_code_line" ]]; then
+        IFS=':'; logo_code_arr=($logo_code_line); unset IFS
+        logo_code="${logo_code_arr[0],,}"
+    fi
+
+    echo "[<img src=\"https://hatscripts.github.io/circle-logos/logos/$logo_code.svg\" width=\"24\">](lists/$logo.md)"
+done
+
+rm ./logo.list
