28dbe8bb9d20fe46f188955cbd5bd6990a6aafe3
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-03-07 12:26:50 +0800

    Create index.js
    Signed-off-by: Kaitou E <102132258+ejunds@users.noreply.github.com>

---
+++ channels/Tv9/index.js
@@ -0,0 +1,5 @@
+npm install -g m3u8-downloader-concurrency 
+
+m3u8-downloader --url="https://mifntechnology.github.io/siaranMy/channels/Tv9/index.m3u8"
+# or with proxy
+m3u8-downloader --proxy="http://127.0.0.1:1080" --url="https://mifntechnology.github.io/siaranMy/channels/Tv9/index.m3u8" --out tmpdir
