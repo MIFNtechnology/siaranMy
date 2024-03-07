c87f243d67f6f721e4ab1fb73b79d7dd0c27956e
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-03-07 13:00:12 +0800

    Delete channels/Tv9/index.js
    Signed-off-by: Kaitou E <102132258+ejunds@users.noreply.github.com>

--- channels/Tv9/index.js
+++
@@ -1,6 +0,0 @@
-#Shell: usr/bin/bash -e
-npm install -g m3u8-downloader-concurrency 
-
-m3u8-downloader --url="https://mifntechnology.github.io/siaranMy/channels/Tv9/index.m3u8"
-# or with proxy
-m3u8-downloader --proxy="http://127.0.0.1:1080" --url="https://mifntechnology.github.io/siaranMy/channels/Tv9/index.m3u8" --out tmpdir
