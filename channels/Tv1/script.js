05a925c8ee330fb805adb11aa55f16f75d7be803
Kaitou E <102132258+ejunds@users.noreply.github.com>
2025-02-16 23:43:07 +0800

    update script.js

--- channels/Tv1/script.js
+++ channels/Tv1/script.js
@@ -1,15 +1,26 @@
-var playerInstance = jwplayer("myElement");
-playerInstance.setup({
-  playlist: [{
-    sources: [{
-      file: "https://mifntechnology.github.io/siaranMy/channels/Tv1/index.m3u8",
-      type: "hls"
-    }],
-    image: "https://mifntechnology.github.io/siaranMy/logo/Tv1.png",
-    title: "TV1",
-    description: "TV1 | RTM"
-  }],
-  width: 640,
-  height: 360,
-  mute: true
-});
+	var initPlayer = function(id,setup) {
+		var gluePlayer = new GluePlyr();
+		gluePlayer.setup(id,setup);
+		return gluePlayer;
+	};
+	var title = "TV1";
+	var img = "https://rtm.glueapi.io/v1/content/1/image/960x540/images.title?c=20230104";
+	var token = "";
+	var playerSettings = {
+		apiPath : "https://rtm.glueapi.io",
+		svgPath : "/latest/includes",
+		watermark : "https://rtm-images.glueapi.io/filters:quality(100)/200x0/system/rtm-watermark.png",
+		target : "player-card-1",
+		poster :  "https://rtm.glueapi.io/v1/content/1/image/960x540/images.title?c=20230104",
+		site : "RTMKlik",
+		type : "video",
+		skin : "0",
+		psa : "",
+		ads : false,
+		casting : true
+	}
+	
+	if(token != "") playerSettings.token = token;
+	window.addEventListener('DOMContentLoaded', function(event){
+		window.gluePlayer = initPlayer(1,playerSettings);
+	});
