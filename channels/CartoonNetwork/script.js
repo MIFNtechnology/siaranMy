8c14f7c27d0ff03847b92ffce8cd3cdae6a40c18
Kaitou E <102132258+ejunds@users.noreply.github.com>
2025-04-25 11:50:16 +0800

    update script.js

--- channels/CartoonNetwork/script.js
+++ channels/CartoonNetwork/script.js
@@ -1,67 +1,1 @@
-const manifestUri = 'https://linearjitp-playback.astro.com.my/dash-wv/linear/509/default_ott.mpd';
-
-    function initApp() {
-      shaka.polyfill.installAll();
-      if (shaka.Player.isBrowserSupported()) {
-        init();
-      } else {
-        console.error('Browser not supported!');
-      }
-    }
-
-    async function init() {
-      const video = document.getElementById('video');
-      const ui = video['ui'];
-      const controls = ui.getControls();
-      const player = controls.getPlayer();
-      player.configure({
-  drm: {
-    clearKeys: {
-      // 'key-id-in-hex': 'key-in-hex',
-        '1a05bebf706408431a390c3f9f40f410': '89c5ff9f8e65c7fe966afbd2f9128e5f'
-    }
-  }
-});
-   player.setTextTrackVisibility(true);
-//player.configure({drm:{servers:{'com.widevine.alpha':'https://license.dstv.com/widevine/getLicense?CrmId=afl&AccountId=afl&ContentId=SH2&SessionId=D3C00F885C24B9C6&Ticket=C839A8D71AB94299'}}}); 
-
-      window.player = player;
-      window.ui = ui;
-      window.controls = controls;
-
-      player.addEventListener("error", onPlayerErrorEvent);
-      controls.addEventListener("error", onUIErrorEvent);
-
-      try {
-        await player.load(manifestUri);
-        console.log('The video has now been loaded!');
-      } catch (error) {
-        onPlayerError(error);
-      }
-
-      function onPlayerErrorEvent(errorEvent) {
-        onPlayerError(event.detail);
-      }
-
-      function onPlayerError(error) {
-        console.error('Error code', error.code + ':', 'Video could not be loaded!', '[Media not found].');
-      }
-
-      function onUIErrorEvent(errorEvent) {
-        onPlayerError(event.detail);
-      }
-    }
-
-    function initFailed(errorEvent) {
-      console.error('Unable to load the UI library!');
-    }
-
-    document.addEventListener('shaka-ui-loaded', init);
-    document.addEventListener('shaka-ui-load-failed', initFailed);
-    document.addEventListener('DOMContentLoaded', initApp);
-
-    Object.defineProperty(navigator, 'userAgent', {
-      get: function() { // Latest (Feb 18, 2024)
-        return 'User Agent: Mozilla/5.0 (Linux; Android 10; CPH1819 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/135.0.7049.99 Mobile Safari/537.36';
-      }
-    });
+async function init() {const video = document.getElementById('video');const ui = video['ui'];const controls = ui.getControls();const player = controls.getPlayer();player.configure({  drm: {    clearKeys: {      // 'key-id-in-hex': 'key-in-hex',        '1a05bebf706408431a390c3f9f40f410': '89c5ff9f8e65c7fe966afbd2f9128e5f'    }  }});   player.setTextTrackVisibility(true);//player.configure({drm:{servers:{'com.widevine.alpha':'https://license.dstv.com/widevine/getLicense?CrmId=afl&AccountId=afl&ContentId=SH2&SessionId=D3C00F885C24B9C6&Ticket=C839A8D71AB94299'}}});window.player = player;window.ui = ui;player.addEventListener('error', onPlayerErrorEvent);controls.addEventListener('error', onUIErrorEvent);try{await player.load('https://linearjitp-playback.astro.com.my/dash-wv/linear/509/default_ott.mpd');console.log('The video has now been loaded!');} catch (error) {onPlayerError(error);}}function onPlayerErrorEvent(errorEvent) {onPlayerError(event.detail);}function onPlayerError(error) {console.error('Error code', error.code, 'object', error);}function onUIErrorEvent(errorEvent) {onPlayerError(event.detail);}function initFailed(errorEvent) {console.error('Unable to load the UI library!');}document.addEventListener('shaka-ui-loaded', init);document.addEventListener('shaka-ui-load-failed', initFailed);
