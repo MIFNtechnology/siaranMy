30766b2dd6b602bf824ed8155e47c11ba6af6611
Kaitou E <102132258+ejunds@users.noreply.github.com>
2025-05-06 09:38:06 +0800

    update script.js

--- channels/TvAlhijrah/script.js
+++ channels/TvAlhijrah/script.js
@@ -1,77 +1,44 @@
-const manifestUri = 'https://linearjitp-playback.astro.com.my/dash-wv/linear/1113/default_ott.mpd';
-
-    function initApp() {
-      shaka.polyfill.installAll();
-      if (shaka.Player.isBrowserSupported()) {
-        init();
-      } else {
-        console.error('Browser not supported!');
-      }
-    }
-
-    async function init() {
-      const video = document.getElementById('video');
-      const ui = video['ui'];
-      const controls = ui.getControls();
-      const player = controls.getPlayer();
-      const config = {
-        'seekBarColors': {
-          base: 'rgba(66, 133, 244, 0.35)',
-          buffered: 'rgba(66, 133, 244, 0.6)',
-          played: 'rgba(66, 133, 244, 0.8)',
-        },
-        'volumeBarColors': {
-          base: 'rgba(66, 133, 244, 0.8)',
-          level: 'rgb(66, 133, 244)',
+async function init() {
+	const video = document.getElementById('video');
+	const ui = video['ui'];
+	const controls = ui.getControls();
+	const player = controls.getPlayer();
+	const config = {
+		enableKeyboardPlaybackControls: true,
+		'seekBarColors': {
+			base: 'rgba(255,255,255,.2)',
+			buffered: 'rgba(255,255,255,.4)',
+			played: 'rgb(255,0,0)',
+          },
+         };
+            ui.configure(config);
+            
+            player.configure({
+            	drm: {
+            	  clearKeys: {
+            	// 'key-id-in-hex': 'key-in-hex',
+            "4745ac4464797aa9d5ed653c1e987917": "ffe2dd898757fb14c156b3e014058ecc"
+            }
+          }
+        });
+        player.getNetworkingEngine().registerRequestFilter(function(type, request) {
+        	if (type == shaka.net.NetworkingEngine.RequestType.SEGMENT) {
+        	//   request.headers['authorization'] = '';
         }
-      }
-      ui.configure(config);
-      player.configure({
-  drm: {
-    clearKeys: {
-      // 'key-id-in-hex': 'key-in-hex',
-        "41a42fb5af457501c11bb9b158d5a110": "57a8d6f9e3832887db18ea6071118736"
-    }
-  }
-});
-
-      window.player = player;
-      window.ui = ui;
-      window.controls = controls;
-
-      player.addEventListener("error", onPlayerErrorEvent);
-      controls.addEventListener("error", onUIErrorEvent);
-
-      try {
-        await player.load(manifestUri);
-        console.log('The video has now been loaded!');
-      } catch (error) {
-        onPlayerError(error);
-      }
-
-      function onPlayerErrorEvent(errorEvent) {
-        onPlayerError(event.detail);
-      }
-
-      function onPlayerError(error) {
-        console.error('Error code', error.code + ':', 'Video could not be loaded!', '[Media not found].');
-      }
-
-      function onUIErrorEvent(errorEvent) {
-        onPlayerError(event.detail);
-      }
-    }
-
-    function initFailed(errorEvent) {
-      console.error('Unable to load the UI library!');
-    }
-
+      });
+      
+      player.setTextTrackVisibility(true);
+    //player.configure({drm:{servers:{'com.widevine.alpha':'https://license.dstv.com/widevine/getLicense?CrmId=afl&AccountId=afl&ContentId=SH2&SessionId=D3C00F885C24B9C6&Ticket=C839A8D71AB94299'}}});
+    
+    window.player = player;
+    window.ui = ui;
+    player.addEventListener('error', onPlayerErrorEvent);
+    controls.addEventListener('error', onUIErrorEvent);
+    try{await player.load('https://unifi-live01.secureswiftcontent.com/UnifiHD/live07.mpd');
+    console.log('The video has now been loaded!');} catch (error) {onPlayerError(error);}}
+    function onPlayerErrorEvent(errorEvent) {onPlayerError(event.detail);}
+    function onPlayerError(error) {console.error('Error code', error.code, 'object', error);}
+    function onUIErrorEvent(errorEvent) {onPlayerError(event.detail);}
+    function initFailed(errorEvent) {console.error('Unable to load the UI library!');}
     document.addEventListener('shaka-ui-loaded', init);
     document.addEventListener('shaka-ui-load-failed', initFailed);
-    document.addEventListener('DOMContentLoaded', initApp);
-
-    Object.defineProperty(navigator, 'userAgent', {
-      get: function() { // Latest (Feb 18, 2024)
-        return 'User Agent: Mozilla/5.0 (Linux; Android 10; CPH1819 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/135.0.7049.99 Mobile Safari/537.36';
-      }
-    });
